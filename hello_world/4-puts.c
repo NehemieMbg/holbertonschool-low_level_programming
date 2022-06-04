612521532c17d357f1d43fdec01c374a60271cb7
NehemieMbg <105085040+NehemieMbg@users.noreply.github.com>
2022-06-04 12:31:37 -0700

    pints followed by a new line

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Program that prints "programming is like building a mutlilinguage puzzle"
+ * followed by a new line 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
