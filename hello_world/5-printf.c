8ddbf8b29937c23bc5f3dd4973eb2330fc52ecfa
NehemieMbg <105085040+NehemieMbg@users.noreply.github.com>
2022-06-04 12:55:11 -0700

    C program that prints

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/** main - prints "exactly with proper grammar, but the outcome is a piece of art,
+/** main - prints the phrases with proper grammar,
+ * but the outcome is a piece of art,
  * followed by a new line 
- * return : 0
+ * return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
