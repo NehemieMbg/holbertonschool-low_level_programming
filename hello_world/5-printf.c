33e4820f4108ae0f8989907b761da1d808e8a6a2
NehemieMbg <105085040+NehemieMbg@users.noreply.github.com>
2022-06-04 12:57:16 -0700

    C program that prints

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/** main - prints the phrases with proper grammar,
+/**
+ * main - prints the phrase with proper grammar,
  * but the outcome is a piece of art,
- * followed by a new line 
- * return: 0
+ * Return: 0,
  */
 int main(void)
 {
