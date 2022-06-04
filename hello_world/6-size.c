afb36208730634ac4541eea52fb5c5ad02722cc0
NehemieMbg <105085040+NehemieMbg@users.noreply.github.com>
2022-06-04 13:24:18 -0700

    ziaké

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,6 @@
  * main - prints the size of various types
  * return: 0 if exited proprely, non-zero otherwise
  */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -12,5 +11,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0)
+	return (0);
 }
