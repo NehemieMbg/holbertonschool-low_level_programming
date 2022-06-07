0e91885f9b12ce575af9a7d6a1dc831bd48c9c11
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-07 20:22:59 +0000

    prints number from 1-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Program that prints alphabet in lowercase but skip
+ * the letter 'q' and 'e'
+ *Return: 0 if succeeded
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
