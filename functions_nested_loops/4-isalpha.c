d984ef2ec0a2c0914bc96c70ad273a0c2ac7bed6
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-09 08:36:52 +0000

    check for carachter

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for carachter
  * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is letter, 0 otherwise
  */
 int _isalpha(int c)
 {
