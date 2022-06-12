67bbdcf1b59fa42b9b9a988d7c59a76ace05e51a
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-12 14:03:38 +0000

    check for digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,9 +3,9 @@
 /**
  * _isdigit - checks for a digit
  * @c: is the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-	return(c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
