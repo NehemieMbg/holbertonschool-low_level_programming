2e54de2c08da39796dc29c9505822741bd71689e
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-12 21:59:01 +0000

    prints numbers from 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints  numbers from 0 to 14,
+ * followed by a new line
+ */
+void more_numbers(void)
+{
+	int n, o;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (o = 0; o <= 14; o++)
+		{
+			if (o >= 10)
+				_putchar(o / 10 + '0');
+			_putchar(o % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
