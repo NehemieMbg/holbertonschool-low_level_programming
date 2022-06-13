046003a3b6b220d8a31c00215ca03ad3715594e1
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-13 13:53:14 +0000

    prints number fro 1 t 100

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,9 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 15 == 0)
+		if (n == 100)
+			printf("Buzz");
+		else if (n % 15 == 0)
 			printf("FizzBuzz ");
 		else if (n % 3 == 0)
 			printf("Fizz ");
