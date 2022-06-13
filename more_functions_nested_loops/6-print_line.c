18fe584bc9385b8dc9df5e9ecc322251856ddf05
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-13 09:41:58 +0000

    print lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - prints "_" line,
+ * @n: result that print lines
+ * followed by a new line
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
