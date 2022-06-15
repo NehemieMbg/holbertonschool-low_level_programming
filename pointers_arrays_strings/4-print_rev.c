7feb6e2bc7cf9b5bbe5c2a0b47355af3c1411d9b
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-15 09:07:30 +0000

    print the string in revers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - Prints the string in reverse
+ * @s: is the pinter that point to the string
+ */
+void print_rev(char *s)
+{
+	int n, i;
+
+	n = 0;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	for (i = n; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
