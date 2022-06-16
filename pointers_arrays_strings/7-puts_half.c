8ed1ba4895deb8c53e469029540dca29e8d89adf
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-16 11:27:38 +0000

    prints the second half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Print the half of a string
+ * @str: is the pointer of the sring
+ */
+void puts_half(char *str)
+{
+	int len, len1;
+
+	/* prints the lenght of a string */
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		;
+	}
+	/* split the variable len in half */
+	len1 = len / 2;
+	/* prints the second half of the variable */
+	while (str[len1] != '\0')
+	{
+		_putchar(str[len1]);
+		len1++;
+	}
+	_putchar('\n');
+}
