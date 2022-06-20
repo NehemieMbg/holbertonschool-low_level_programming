329b7856dc1e8a9af64e7e842e1a7de39ae24e88
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-20 09:04:51 +0000

    Fuctions

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - concanates two strings.
+ * @dest: The final destination which will retur the result
+ * @src: The source of other string
+ *
+ * Return: the pointer to dest
+ */
+void _puts_recursion(char *s)
+{
+	
+}
