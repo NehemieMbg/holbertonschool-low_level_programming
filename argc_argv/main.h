e3d21074866c78dcf10d2887fc08f3973d55098f
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-22 08:37:36 +0000

    Functions

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -17,3 +17,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]__attribute__((unused)));
+int main(int argc, char *argv[]);
