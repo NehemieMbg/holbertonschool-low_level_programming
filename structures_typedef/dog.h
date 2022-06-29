55ccc36ba10cab8ab91bc8a89fc759f45d59d4e9
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-29 12:20:02 +0000

    Define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - is the new type defined
+ * @name: is the name to assign
+ * @age: is the age to assign
+ * @owner: is the owner to assign
+ */
+
 struct dog
 {
 	char *name;
