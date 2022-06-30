40c4485f1f912c253784bcffdd535888339e512c
NehemieMbg <nehemie.mbg@gmail.com>
2022-06-30 14:02:58 +0000

    Funciton that prints name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Function that point to a function given by an argument
+ * @f: is the pointed function
+ * @name: is the string to print
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
