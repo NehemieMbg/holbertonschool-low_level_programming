f181717c1c2986475b8069b76c5a0ae0dc4951d3
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-01 18:09:34 +0000

    main c

---
+++ function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - addition two numbers
+ * @a: integer to opperate
+ * @b: integer to opperate
+ * Return: the final value
+ */
+
+int op_add(int a, int b)
