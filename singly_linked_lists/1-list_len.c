16316603745dd340caf94d1b4a3db2eb57b0876e
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-12 14:57:13 +0000

    returns the number of elements

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - return the number of elements in a linked lst
+ *
+ * @h: is the pointer to the next list
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t length;
+
+	length = 0;
+
+	while (h)
+	{
+		length++;
+		h = h ->next;
+	}
+
+	return (length);
+}
