4a0314732735cdce0b7cca1767c28b8b43f4a638
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-13 10:53:39 +0000

    Function that frees list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,8 +1,13 @@
 #include "lists.h"
 
+/**
+ * free_list - frees a list
+ *
+ * @head: is a pointer that point to the next node
+ */
 void free_list(list_t *head)
 {
- 	list_t *temp;
+	list_t *temp;
 
 	while (head)
 	{
