71346d5ab5928b02e59ef5d89930cfaca5c59392
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-13 08:46:57 +0000

    give the length to len from str

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,11 +11,13 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+
 	if (!new)
 		return (NULL);
 
 
 	new->str = strdup(str);
+	new->len = strlen(str);
 	new->next = NULL;
 
 	if (*head == NULL)
