24ef9bfc4d35007d00f41510cc1a1e479eb12db5
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-14 19:33:18 +0000

    Function that frees list

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint - Function that frees list that been alocated
+ * with memory.
+ *
+ * @head: Is the pointer that point to the head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	/**
+	 * protecting my head pointer by creating a
+	temporary pointer and giving him the same data
+	 */
+	listint_t *tmp;
+
+	/*creating a loop that gonna free the rest of the list*/
+	while (head == NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+
+	/*there is nothing to return since the function is a void format*/
+}
