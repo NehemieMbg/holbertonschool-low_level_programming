99ebd34d996f43cea41828a050e8e0635f053ad8
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-15 15:10:47 +0000

    test

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if(!*head)
 		return(NULL);
 
+	if (idx == 0)
+	{
+		newnode->next = *head;
+		*head = newnode;
+	}
 	while (tmp && count < idx)
 	{
 		tmp = tmp->next;
