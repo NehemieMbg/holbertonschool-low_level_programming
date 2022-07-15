2cda3b884bfb77827b684435fba74204d5d2ab33
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-15 15:19:05 +0000

    test

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,8 +19,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!newnode)
 		return(NULL);
 
+
 	if(!*head)
-		return(NULL);
+		*head = newnode;
 
 	tmp = *head;
 	newnode->n = n;
