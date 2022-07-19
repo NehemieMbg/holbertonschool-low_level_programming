5c6ca75a3a9cc6d8c25d4c3d9f5cdbc577d465ce
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-19 09:52:06 +0000

    Prototype modified

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -7,7 +7,7 @@
 /*Prototypes*/
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int decimalNo);
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int number, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
