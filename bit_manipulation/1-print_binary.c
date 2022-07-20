285654280bc6d23a9166f59dab0941dd28faec0d
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-20 08:30:34 +0000

    Removing 0 before binary code

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -9,29 +9,7 @@
 
 void print_binary(unsigned long int decimalNo)
 {
-	int status = 0;
-	unsigned int mask = 65536;
-
-	if (decimalNo == 0)
-		_putchar('0');
-
-	else
-	{
-		while (mask > 0)
-		{
-			if ((decimalNo & mask) == 0)
-			{
-				if (status != 0)
-					_putchar('0');
-			}
-
-			else
-			{
-				_putchar('1');
-				status = 1;
-			}
-
-			mask = mask >> 1;
-		}
-	}
+	if (decimalNo > 1)
+		print_binary(decimalNo >> 1);
+	_putchar((decimalNo & 1) + '0');
 }
