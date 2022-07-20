65c993ea44a28ec211568a10b0ffc4ff97d1edd1
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-20 13:41:40 +0000

    Description fixed

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * set_bit - sets the value of a bit
+ * @n: is the number in decimal
+ * @index: is where we want to set a bit
+ *
+ * Return: (1) if it worked, (-1) if it didn't
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 32)
 		return (-1);
-	
+
 	*n |= (1 << index);
-	
-	return(1);
+
+	return (1);
 }
