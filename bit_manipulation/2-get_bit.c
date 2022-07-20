6211725eabd9858644d2156f671015a3e4429970
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-20 08:47:53 +0000

    Returns the value of a bit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
 /**
- * @brief 
- * 
+ * get_bit - Function that return the value of at
+ * given index
+ *
+ * @index: is the bit to get
+ * @number: is the decimal number
+ *
+ * Return: The value
  */
 
 int get_bit(unsigned long int number, unsigned int index)
