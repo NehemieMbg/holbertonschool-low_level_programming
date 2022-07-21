3c1790f1291d985b5c38d0f490dc1c44f1b10581
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-21 14:44:30 +0000

    Function that create file"

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,6 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (creat_ == -1)
 		return (-1);
 
+	if (!text_content)
+		return (-1);
+
 	write_ = write(creat_, text_content, strlen(text_content));
 	if (write_ == -1)
 		return (-1);
