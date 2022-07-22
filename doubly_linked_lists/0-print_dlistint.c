ad0064d834435322421e4f9fcddd954d84bcea69
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-22 08:55:12 +0000

    prints all elements of a list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Function that prints all the elements of a list
+ *
+ * @h: is the the header of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
