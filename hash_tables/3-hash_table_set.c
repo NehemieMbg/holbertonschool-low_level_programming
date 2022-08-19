53482e419db49ccdb6fea9664c8d3f8fc12ff4c5
NehemieMbg <nehemie.mbg@gmail.com>
2022-08-19 15:50:04 +0000

    new function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,8 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * @brief 
- * 
+ * hash_table_set - adds an element to the hash table
+ * @ht: is the hash table to update
+ * @key: is the key
+ * @value: is the value associated with the key
+ * Return: 1 if succeeded 0 otherwise
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -11,21 +14,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *temp;
 	hash_node_t *newNode;
 
-	if(!key || strcmp(key, "") == 0 || value || ht)
+	if (!key || strcmp(key, "") == 0 || value || ht)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
 	temp = ht->array[index];
 
-	while(temp)
+	while (temp)
 	{
-		if(strcmp(key, temp->key) == 0)
+		if (strcmp(key, temp->key) == 0)
 		{
 			free(temp->value);
 			temp->value = strdup(value);
 
-			if(!temp->value)
+			if (!temp->value)
 				return (0);
 
 			return (1);
@@ -35,9 +38,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	newNode = malloc(sizeof(hash_node_t));
 
-	if(!newNode)
-		return(0);
-	
+	if (!newNode)
+		return (0);
+
 	newNode->key = strdup(key);
 	newNode->value = strdup(value);
 	newNode->next = ht->array[index];
