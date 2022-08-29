f76a973f736d622b722478b74f640edeef27d3a5
NehemieMbg <nehemie.mbg@gmail.com>
2022-08-29 15:10:26 +0000

    new function

---
+++ search_algorithms/100-jump.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t jump = sqrt(size), index = 0;
+	size_t low = 0, high = 0;
+
+	while (index < size)
+	{
+		if (array[index] >= value)
+			break;
+		else
+			printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		index += jump;
+	}
+	low = index - jump;
+	high = index; 
+	index = low;
+
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+
+	while (index <= high && index < size)
+	{
+		printf("Value checked array [%lu] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+		
+		index++;
+	}
+	return (-1);
+}
